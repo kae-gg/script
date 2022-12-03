b64fd99d23b2f83338bcb3425528a9f0f2608419
kae-gg <119721055+kae-gg@users.noreply.github.com>
2022-12-03 14:02:16 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,13 @@
+local games = {
+    ["3965173814"] = "swordslasher"
+}
+
+local supported = games[game.GameId]
+
+if supported then
+    game:GetService("Players").LocalPlayer.Idled:Connect(function()
+        game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    end)
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/kae-gg/script/main/" .. supported .. ".lua"))()
+end
